19f6a1e73bc70c08c45a0ead1c9cd54d8a3a7f45
L <eleaz.rs@gmail.com>
2023-09-21 16:42:01 +0200

    please, accounts in plural 😎

--- src/main/java/org/ldcgc/backend/controller/users/AccountController.java
+++ src/main/java/org/ldcgc/backend/controller/users/AccountController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/account")
+@RequestMapping("/accounts")
 public interface AccountController {
 
     // TODO
