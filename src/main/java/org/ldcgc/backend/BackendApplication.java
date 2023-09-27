6b2cd9c1e7d3d153fad80094223542553e13dd43
L <eleaz.rs@gmail.com>
2023-09-28 00:11:40 +0200

    enable transaction management for app

--- src/main/java/org/ldcgc/backend/BackendApplication.java
+++ src/main/java/org/ldcgc/backend/BackendApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class BackendApplication {
 
@@ -17,4 +19,5 @@ public class BackendApplication {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
 }
