ff145eae5d3937ce0c8820449e674a4c88276f16
L <eleaz.rs@gmail.com>
2023-10-02 14:24:06 +0200

    clean unused classes since environment by default is set

--- src/test/java/org/ldcgc/backend/BackendApplicationTests.java
+++ src/test/java/org/ldcgc/backend/BackendApplicationTests.java
@@ -4,12 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
-@ExtendWith(GlobalTestConfig.class)
 class BackendApplicationTests {
 
 	@Test
