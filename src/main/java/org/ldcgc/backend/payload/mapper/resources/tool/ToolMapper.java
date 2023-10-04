6cd5d15e6d6613d03026b564e064172d55bdc503
L <eleaz.rs@gmail.com>
2023-10-04 18:11:42 +0200

    clean and set Mappers

---
+++ src/main/java/org/ldcgc/backend/payload/mapper/resources/tool/ToolMapper.java
@@ -0,0 +1,24 @@
+package org.ldcgc.backend.payload.mapper.resources.tool;
+
+import org.ldcgc.backend.db.model.resources.Tool;
+import org.ldcgc.backend.payload.dto.resources.ToolDto;
+import org.ldcgc.backend.payload.mapper.category.CategoryMapper;
+import org.ldcgc.backend.payload.mapper.location.LocationMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(uses = { LocationMapper.class, CategoryMapper.class },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface ToolMapper {
+
+    ToolMapper MAPPER = Mappers.getMapper(ToolMapper.class);
+
+    @Mapping(source = "status.name", target = "status")
+    ToolDto toDto(Tool tool);
+
+    @Mapping(target = "status", ignore = true)
+    Tool toMo(ToolDto toolDto);
+
+}
