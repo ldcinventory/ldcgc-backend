10f3c9fe27a2d96c7ea3ed7017b7657d7b7eb3ec
L <eleaz.rs@gmail.com>
2023-11-30 15:34:29 +0100

    more messages for Application

--- src/main/java/org/ldcgc/backend/util/retrieving/Message.java
+++ src/main/java/org/ldcgc/backend/util/retrieving/Message.java
@@ -11,16 +11,25 @@ public class Message implements EnumMethods {
     @Getter
     @RequiredArgsConstructor
     public enum InfoMessage implements EnumMethods {
+        RECOVERY_TOKEN_VALID("Recovery token valid"),
 
         USER_CREATED("User created successfully!"),
         USER_LISTED("Found %s user/s"),
         USER_UPDATED("User details updated"),
+        USER_CREDENTIALS_UPDATED("User credentials updated"),
         USER_DELETED("User deleted"),
+
         VOLUNTEER_CREATED("Volunteer created"),
         VOLUNTEER_LISTED("Found %s volunteer/s"),
         VOLUNTEER_UPDATED("Volunteer details updated"),
         VOLUNTEER_DELETED("Volunteer deleted"),
-        LOGOUT_SUCCESSFUL("Logout successful");
+
+        CREDENTIALS_EMAIL_SENT("Credentials email sent"),
+        EMAIL_SENT("Email sent"),
+
+        LOGOUT_SUCCESSFUL("Logout successful")
+
+        ;
 
         private final String message;
 
@@ -30,7 +39,10 @@ public class Message implements EnumMethods {
     @RequiredArgsConstructor
     public enum AppMessage implements EnumMethods {
 
-        CREDENTIALS_EMAIL_TEMPLATE("credentialsTemplate.html");
+        CREDENTIALS_EMAIL_TEMPLATE("credentialsTemplate.html"),
+        CREDENTIALS_RECOVERY_SUBJECT("GC8Inventory : [Recuperación de credenciales] 🙃")
+
+        ;
 
         private final String message;
 
@@ -42,11 +54,15 @@ public class Message implements EnumMethods {
         TOKEN_NOT_PARSEABLE("This token is not parseable"),
         TOKEN_NOT_VALID("This token is not valid"),
         TOKEN_NOT_FOUND("This token doesn't exist in DB"),
+        JWT_NOT_FOR_RECOVERY("This token is not for recover the account. Sorry, mate!"),
+        RECOVERY_TOKEN_NOT_VALID_NOT_FOUND("This recovery token is not valid or is not found"),
 
         USER_ALREADY_EXIST("There's a user with this id or email"),
         USER_NOT_FOUND("User not found"),
         USER_NOT_FOUND_TOKEN("User id or user from token not found, or token is not valid"),
         USER_PASSWORD_DOESNT_MATCH("Password provided for this email doesn't match our records"),
+
+        EMAIL_NOT_SENT("Email not sent!"),
 
         VOLUNTEER_ALREADY_EXIST("There's a volunteer with this id"),
         VOLUNTEER_NOT_FOUND("The volunteer you're searching for with this id couldn't be found"),
@@ -56,7 +72,9 @@ public class Message implements EnumMethods {
         STATUS_NOT_FOUND("Status not found"),
 
         RUNTIME_EXCEPTION("Error processing data, check your request"),
-        ENDPOINT_NOT_IMPLEMENTED("This endpoint is not implemented yet");
+        ENDPOINT_NOT_IMPLEMENTED("This endpoint is not implemented yet")
+
+        ;
 
         private final String message;
 
