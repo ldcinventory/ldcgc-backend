a142dd3153fbf028040dba26b57e7a14e37f1cab
L <eleaz.rs@gmail.com>
2023-12-01 10:16:10 +0100

    fix updateUser

--- src/main/java/org/ldcgc/backend/service/users/impl/UserServiceImpl.java
+++ src/main/java/org/ldcgc/backend/service/users/impl/UserServiceImpl.java
@@ -12,7 +12,6 @@ import org.ldcgc.backend.payload.mapper.users.UserMapper;
 import org.ldcgc.backend.security.jwt.JwtUtils;
 import org.ldcgc.backend.service.users.UserService;
 import org.ldcgc.backend.util.creation.Constructor;
-import org.ldcgc.backend.util.mock.UserMock;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -52,17 +51,9 @@ public class UserServiceImpl implements UserService {
     }
 
     public ResponseEntity<?> updateMyUser(String token, UserDto user) throws ParseException {
-
         Integer userId = jwtUtils.getUserIdFromStringToken(token);
 
-        User userEntity = userRepository.findById(userId).orElseThrow(() ->
-            new RequestException(HttpStatus.NOT_FOUND, getErrorMessage(USER_NOT_FOUND)));
-
-        UserMapper.MAPPER.update(user, userEntity);
-
-        userRepository.saveAndFlush(userEntity);
-
-        return Constructor.buildResponseMessage(HttpStatus.OK, getInfoMessage(USER_UPDATED));
+        return updateUser(userId, user);
     }
 
     public ResponseEntity<?> deleteMyUser(String token) throws ParseException {
@@ -110,10 +101,13 @@ public class UserServiceImpl implements UserService {
     }
 
     public ResponseEntity<?> updateUser(Integer userId, UserDto user) {
-        if (!userRepository.existsById(userId))
-            throw new RequestException(HttpStatus.NOT_FOUND, getErrorMessage(USER_NOT_FOUND));
+        User userEntity = userRepository.findById(userId).orElseThrow(() ->
+            new RequestException(HttpStatus.NOT_FOUND, getErrorMessage(USER_NOT_FOUND)));
 
-        return Constructor.buildResponseMessageObject(HttpStatus.OK, getInfoMessage(USER_UPDATED), UserMock.getMockedUser(userId));
+        UserMapper.MAPPER.update(user, userEntity);
+        userRepository.save(userEntity);
+
+        return Constructor.buildResponseMessageObject(HttpStatus.OK, getInfoMessage(USER_UPDATED), UserMapper.MAPPER.toDTO(userEntity));
     }
 
     public ResponseEntity<?> deleteUser(Integer userId) {
