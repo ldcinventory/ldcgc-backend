d5c8a2415fa453d70345304c9e5ad1254a3736c6
L <eleaz.rs@gmail.com>
2023-12-07 09:58:06 +0100

    force every log and message go in UTF-8

--- src/main/java/org/ldcgc/backend/GC8InventoryBackend.java
+++ src/main/java/org/ldcgc/backend/GC8InventoryBackend.java
@@ -8,6 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 @Slf4j
 @EnableTransactionManagement
 @SpringBootApplication
@@ -25,6 +30,13 @@ public class GC8InventoryBackend {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GC8InventoryBackend.class, args);
+
+		try {
+			System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			log.error("VM does not support mandatory encoding UTF-8");
+			throw new InternalError("VM does not support mandatory encoding UTF-8");
+		}
 
 		log.info("Api running! Check swagger here: http://localhost:{}/api/swagger-ui/index.html",
 			INSTANCE.contextConstants.getTomcatPort());
