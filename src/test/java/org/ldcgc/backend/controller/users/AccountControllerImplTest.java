bfbc54c892dbb49c4763e4a4533b6d6520c424cd
L <eleaz.rs@gmail.com>
2023-12-11 13:54:02 +0100

    first approach with account controller tests

---
+++ src/test/java/org/ldcgc/backend/controller/users/AccountControllerImplTest.java
@@ -0,0 +1,142 @@
+package org.ldcgc.backend.controller.users;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
+import org.hibernate.validator.HibernateValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.ldcgc.backend.base.annotation.TestConstrainValidationFactory;
+import org.ldcgc.backend.configuration.ContextConstants;
+import org.ldcgc.backend.db.repository.users.TokenRepository;
+import org.ldcgc.backend.db.repository.users.UserRepository;
+import org.ldcgc.backend.payload.dto.users.UserDto;
+import org.ldcgc.backend.security.jwt.JwtUtils;
+import org.ldcgc.backend.security.user.UserDetailsServiceImpl;
+import org.ldcgc.backend.service.users.AccountService;
+import org.ldcgc.backend.util.common.ERole;
+import org.ldcgc.backend.validator.UserValidation;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.ldcgc.backend.base.factory.TestRequestFactory.postRequest;
+import static org.ldcgc.backend.base.mock.MockedUserDetails.getRandomMockedUserDto;
+import static org.ldcgc.backend.base.mock.MockedUserDetails.getRandomMockedUserDtoLogin;
+import static org.ldcgc.backend.util.retrieving.Message.InfoMessage.LOGOUT_SUCCESSFUL;
+import static org.ldcgc.backend.util.retrieving.Message.getInfoMessage;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = AccountController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+class AccountControllerImplTest {
+    // controller
+    @Autowired private AccountController accountController;
+    // services
+    @MockBean private AccountService accountService;
+    @MockBean private UserDetailsServiceImpl userDetailsService;
+    @MockBean private JwtUtils jwtUtils;
+    // repositories
+    @MockBean private TokenRepository tokenRepository;
+    @MockBean private UserRepository userRepository;
+    // other
+    @MockBean private UserValidation userValidation;
+    @MockBean private ContextConstants contextConstants;
+    // mapper
+    @Autowired private ObjectMapper mapper;
+
+    private MockMvc mockMvc;
+    private UserDto mockedUser;
+    private UserDto mockedUserLogin;
+    private TestConstrainValidationFactory constrainValidationFactory;
+
+    @BeforeEach
+    public void init() {
+
+        final GenericWebApplicationContext context = new GenericWebApplicationContext(new MockServletContext());
+        final ConfigurableListableBeanFactory beanFactory = ((ConfigurableApplicationContext) context).getBeanFactory();
+        beanFactory.registerSingleton(UserValidation.class.getCanonicalName(), UserValidation.bean(userRepository, jwtUtils));
+        context.refresh();
+
+        LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
+        validatorFactoryBean.setApplicationContext(context);
+        constrainValidationFactory = new TestConstrainValidationFactory(context);
+        validatorFactoryBean.setConstraintValidatorFactory(constrainValidationFactory);
+        validatorFactoryBean.setProviderClass(HibernateValidator.class);
+        validatorFactoryBean.afterPropertiesSet();
+
+        mockMvc = MockMvcBuilders
+            .standaloneSetup(accountController)
+            .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
+            .setValidator(validatorFactoryBean)
+            .setHandlerExceptionResolvers()
+            .build();
+
+        mockedUser = getRandomMockedUserDto();
+        mockedUserLogin = getRandomMockedUserDtoLogin();
+
+    }
+
+    @Test
+    public void authenticateUserWithValidCredentials() throws Exception {
+
+        given(accountService.login(ArgumentMatchers.any())).willAnswer(
+            invocation -> ResponseEntity.status(HttpStatus.OK).body(mockedUser));
+
+        mockMvc.perform(postRequest("/accounts/login")
+                .content(mapper.writeValueAsString(mockedUserLogin)))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id", CoreMatchers.is(mockedUser.getId())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(mockedUser.getEmail())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.role", CoreMatchers.containsStringIgnoringCase(mockedUser.getRole().getRoleName())))
+            .andExpect(content().encoding(StandardCharsets.UTF_8));
+
+    }
+
+    @Test
+    //@WithMockUser(username="admin",roles={"USER","MANAGER","ADMIN"})
+    public void logoutUser() throws Exception {
+
+        // validation
+        given(jwtUtils.getUserIdFromStringToken(Mockito.anyString())).willReturn(0);
+
+        given(userRepository.existsById(Mockito.anyInt())).willReturn(Boolean.TRUE);
+
+        given(userValidation.userFromTokenExistsInDB(Mockito.anyString())).willReturn(Boolean.TRUE);
+
+        given(accountService.logout(ArgumentMatchers.any())).willAnswer(
+            invocation -> ResponseEntity.status(HttpStatus.OK).body(getInfoMessage(LOGOUT_SUCCESSFUL)));
+
+        ReflectionTestUtils.setField(constrainValidationFactory, "isValid", true);
+
+        mockMvc.perform(postRequest("/accounts/logout", ERole.ROLE_USER))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(getInfoMessage(LOGOUT_SUCCESSFUL)))
+            .andExpect(content().encoding(StandardCharsets.UTF_8));
+
+    }
+
+}
