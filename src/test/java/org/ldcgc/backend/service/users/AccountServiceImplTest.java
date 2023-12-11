204375ae64e54c446158628a978892c7c069b884
L <eleaz.rs@gmail.com>
2023-12-11 16:45:03 +0100

    set all methods to test the account service class

--- src/test/java/org/ldcgc/backend/service/users/AccountServiceImplTest.java
+++ src/test/java/org/ldcgc/backend/service/users/AccountServiceImplTest.java
@@ -1,8 +1,106 @@
 package org.ldcgc.backend.service.users;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.ldcgc.backend.payload.dto.users.UserDto;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.nio.charset.StandardCharsets;
+
+import static org.ldcgc.backend.base.mock.MockedUserDetails.getRandomMockedUserDto;
+import static org.ldcgc.backend.base.mock.MockedUserDetails.getRandomMockedUserDtoLogin;
+
+@Slf4j
 @SpringBootTest
 class AccountServiceImplTest {
+    // service
+    @MockBean private AccountService accountService;
+
+    private MockMvc mockMvc;
+    private UserDto mockedUser;
+    private UserDto mockedUserLogin;
+
+    @BeforeEach
+    public void init() {
+
+        mockMvc = MockMvcBuilders
+            .standaloneSetup(accountService)
+            .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
+            .setHandlerExceptionResolvers()
+            .build();
+
+        mockedUser = getRandomMockedUserDto();
+        mockedUserLogin = getRandomMockedUserDtoLogin();
+
+    }
+
+    // -> login
+    @Test
+    public void authenticateUser() {
+
+    }
+
+    @Test
+    public void authenticateUserEmailIsInvalid() {
+
+    }
+
+    @Test
+    public void authenticateUserPasswordIsInvalid() {
+
+    }
+
+    // -> logout
+    @Test
+    public void logoutUser() {
+
+    }
+
+    // -> recover
+    @Test
+    public void recoverCredentials() {
+
+    }
+
+    @Test
+    public void recoverCredentialsUserNotFound() {
+
+    }
+
+    // -> validate token
+    @Test
+    public void validateTokenWhenRecoveringCredentials() {
+
+    }
+
+    @Test
+    public void validateTokenWhenRecoveringCredentialsTokenNotExists() {
+
+    }
+
+    @Test
+    public void validateTokenWhenRecoveringCredentialsTokenNotForRecovery() {
+
+    }
+
+    @Test
+    public void validateTokenWhenRecoveringCredentialsUserNotFound() {
+
+    }
+
+    // -> new credentials
+    @Test
+    public void setNewPasswordWhenRecoveringCredentials() {
+
+    }
+
+    @Test
+    public void setNewPasswordWhenRecoveringCredentialsUserNotFound() {
+
+    }
 
 }
