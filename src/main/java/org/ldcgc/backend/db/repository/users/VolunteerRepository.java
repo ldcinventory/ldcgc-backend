ac7f7be37f8190d83e6049b2543bc389898c4636
L <eleaz.rs@gmail.com>
2023-12-12 19:33:53 +0100

    add method to find all filtered volunteers

--- src/main/java/org/ldcgc/backend/db/repository/users/VolunteerRepository.java
+++ src/main/java/org/ldcgc/backend/db/repository/users/VolunteerRepository.java
@@ -1,12 +1,22 @@
 package org.ldcgc.backend.db.repository.users;
 
 import org.ldcgc.backend.db.model.users.Volunteer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface VolunteerRepository extends JpaRepository<Volunteer, Integer> {
 
     Optional<Volunteer> findByBuilderAssistantId(String builderAssistantId);
+
+    @Query("""
+            SELECT v FROM Volunteer v
+            WHERE LOWER(v.name) LIKE LOWER(CONCAT('%', :filterString,'%'))
+              AND LOWER(v.lastName) LIKE LOWER(CONCAT('%', :filterString,'%'))
+            """)
+    Page<Volunteer> findAllFiltered(String filterString, Pageable pageable);
 
 }
