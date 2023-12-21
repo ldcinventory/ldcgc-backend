9aa2a3b6d55cae77782723485f5ba0e6f7516d40
L <eleaz.rs@gmail.com>
2023-12-21 16:14:16 +0100

    add mapper to userCredentialsDto into UserMapper

--- src/main/java/org/ldcgc/backend/payload/mapper/users/UserMapper.java
+++ src/main/java/org/ldcgc/backend/payload/mapper/users/UserMapper.java
@@ -1,6 +1,7 @@
 package org.ldcgc.backend.payload.mapper.users;
 
 import org.ldcgc.backend.db.model.users.User;
+import org.ldcgc.backend.payload.dto.users.UserCredentialsDto;
 import org.ldcgc.backend.payload.dto.users.UserDto;
 import org.ldcgc.backend.payload.mapper.category.CategoryMapper;
 import org.ldcgc.backend.payload.mapper.location.LocationMapper;
@@ -22,6 +23,8 @@ public interface UserMapper {
     @Mapping(target = "password", ignore = true)
     @Mapping(target = "group.location.locations", ignore = true)
     UserDto toDTO(User user);
+
+    UserCredentialsDto toCredentialsDTO(User user);
 
     @Mapping(source = "password", target = "password", qualifiedByName = "mapPasswordToEncryptedPassword")
     void update(UserDto userFrom, @MappingTarget User userTo);
