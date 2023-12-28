7a847567d330bd919e7e594f8df116a7a09e18fe
L <eleaz.rs@gmail.com>
2023-12-28 13:26:01 +0100

    avoid mapping id from dto to entity

--- src/main/java/org/ldcgc/backend/payload/mapper/users/UserMapper.java
+++ src/main/java/org/ldcgc/backend/payload/mapper/users/UserMapper.java
@@ -18,6 +18,7 @@ public interface UserMapper {
 
     UserMapper MAPPER = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "group.location.locations", ignore = true)
     User toEntity(UserDto userRequest);
 
