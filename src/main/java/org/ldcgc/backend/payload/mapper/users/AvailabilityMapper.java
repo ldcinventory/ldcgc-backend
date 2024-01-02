0a72ee02ee4224100e7196c0e755e29267d2a3fd
L <eleaz.rs@gmail.com>
2024-01-02 13:08:18 +0100

    fix mapping nested classes

--- src/main/java/org/ldcgc/backend/payload/mapper/users/AvailabilityMapper.java
+++ src/main/java/org/ldcgc/backend/payload/mapper/users/AvailabilityMapper.java
@@ -1,9 +1,12 @@
 package org.ldcgc.backend.payload.mapper.users;
 
 import org.ldcgc.backend.db.model.users.Availability;
+import org.ldcgc.backend.db.model.users.Volunteer;
 import org.ldcgc.backend.payload.dto.users.AvailabilityDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
@@ -14,10 +17,17 @@ public interface AvailabilityMapper {
 
     AvailabilityMapper MAPPER = Mappers.getMapper(AvailabilityMapper.class);
 
+    @Mapping(target = "volunteer", source = "volunteerId", qualifiedByName = "mapVolunteerToVolunteerId")
     Availability toEntity(AvailabilityDto availabilityDto);
 
+    @Mapping(target = "volunteerId", source = "volunteer.id")
     AvailabilityDto toDto(Availability availability);
 
     void update(@MappingTarget Availability availability, AvailabilityDto availabilityDto);
+
+    @Named("mapVolunteerToVolunteerId")
+    static Volunteer mapVolunteerToVolunteerId(Integer volunteerId) {
+        return Volunteer.builder().id(volunteerId).build();
+    }
 
 }
