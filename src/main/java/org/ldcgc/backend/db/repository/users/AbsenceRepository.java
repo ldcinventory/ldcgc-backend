6f2cf15efbea5d77debeda4b940f04ac43cd0eaa
L <eleaz.rs@gmail.com>
2024-01-06 18:24:39 +0100

    implements some interfaces to user CriteriaBuilder

--- src/main/java/org/ldcgc/backend/db/repository/users/AbsenceRepository.java
+++ src/main/java/org/ldcgc/backend/db/repository/users/AbsenceRepository.java
@@ -2,7 +2,12 @@ package org.ldcgc.backend.db.repository.users;
 
 import org.ldcgc.backend.db.model.users.Absence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
 
-public interface AbsenceRepository extends JpaRepository<Absence, Integer> {
+public interface AbsenceRepository extends
+    JpaRepository<Absence, Integer>,
+    CrudRepository<Absence, Integer>,
+    JpaSpecificationExecutor<Absence> {
 
 }
