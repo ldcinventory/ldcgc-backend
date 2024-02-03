f162c508bebeb11e19c5985640ad22943451e73b
L <eleaz.rs@gmail.com>
2024-02-04 00:36:06 +0100

    clean imports

--- src/main/java/org/ldcgc/backend/db/repository/group/GroupRepository.java
+++ src/main/java/org/ldcgc/backend/db/repository/group/GroupRepository.java
@@ -1,7 +1,6 @@
 package org.ldcgc.backend.db.repository.group;
 
 import org.ldcgc.backend.db.model.group.Group;
-import org.ldcgc.backend.db.model.location.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
--- src/main/java/org/ldcgc/backend/util/creation/Constructor.java
+++ src/main/java/org/ldcgc/backend/util/creation/Constructor.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
 
 @Component
 public class Constructor {
