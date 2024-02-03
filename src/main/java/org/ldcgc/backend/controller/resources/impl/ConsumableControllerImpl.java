cdda1ec28d1e08cc8c07d4ec5b984db86e3f5617
L <eleaz.rs@gmail.com>
2024-02-04 00:13:05 +0100

    remove @Autowired

--- src/main/java/org/ldcgc/backend/controller/resources/impl/ConsumableControllerImpl.java
+++ src/main/java/org/ldcgc/backend/controller/resources/impl/ConsumableControllerImpl.java
@@ -1,21 +1,18 @@
 package org.ldcgc.backend.controller.resources.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.ldcgc.backend.controller.resources.ConsumableController;
 import org.ldcgc.backend.payload.dto.resources.ConsumableDto;
-import org.ldcgc.backend.service.category.CategoryService;
 import org.ldcgc.backend.service.resources.consumable.ConsumableService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
 public class ConsumableControllerImpl implements ConsumableController{
-    @Autowired
-    private ConsumableService consumableService;
 
-    @Autowired
-    private CategoryService categoryService;
+    private final ConsumableService consumableService;
 
     public ResponseEntity<?> getConsumable(Integer consumableId) {
         return consumableService.getConsumable(consumableId);
