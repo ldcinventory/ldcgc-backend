02dc569a9209b96c9676c091c073cdfb2d911b5f
L <eleaz.rs@gmail.com>
2024-02-04 01:56:35 +0100

    add method to find all categories by parent name

--- src/main/java/org/ldcgc/backend/db/repository/category/CategoryRepository.java
+++ src/main/java/org/ldcgc/backend/db/repository/category/CategoryRepository.java
@@ -3,8 +3,8 @@ package org.ldcgc.backend.db.repository.category;
 import org.ldcgc.backend.db.model.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
@@ -12,7 +12,9 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
     Optional<Category> findByName(String name);
 
     @Query("SELECT c.id FROM Category c WHERE c.name = :name")
-    Integer findCategoryIdByName(@Param("name") String name);
+    Integer findCategoryIdByName(String name);
+
+    List<Category> findAllByParent_Name(String name);
 
     Optional<Category> getCategoryByName(String name);
 
