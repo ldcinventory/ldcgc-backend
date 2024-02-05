27aac97a973acc4cfad8a21a44a2fc93f34d79f9
L <eleaz.rs@gmail.com>
2024-02-05 22:51:10 +0100

    add enum for time unit

--- src/main/java/org/ldcgc/backend/util/common/ETimeUnit.java
+++ src/main/java/org/ldcgc/backend/util/common/ETimeUnit.java
@@ -12,26 +12,27 @@ import java.util.Arrays;
 @Getter
 @RequiredArgsConstructor
 public enum ETimeUnit implements EnumMethods {
-    DAYS ("días", 1),
-    WEEKS ("semanas", 2),
-    MONTHS ("meses", 3),
-    YEARS ("años", 4);
+
+    DAYS("días", 1),
+    WEEKS("semanas", 2),
+    MONTHS("meses", 3),
+    YEARS("años", 4);
 
     private final String desc;
     private final Integer id;
 
     public static ETimeUnit getTimeUnitFromId(Integer id) {
         return Arrays.stream(ETimeUnit.values())
-                .filter(status -> status.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new RequestException(HttpStatus.NOT_FOUND, String.format(Messages.Error.TIME_UNIT_NOT_FOUND, id)));
+            .filter(status -> status.getId().equals(id))
+            .findFirst()
+            .orElseThrow(() -> new RequestException(HttpStatus.NOT_FOUND, String.format(Messages.Error.TIME_UNIT_NOT_FOUND, id)));
     }
 
-    public static ETimeUnit getTimeUnitByName(String name){
+    public static ETimeUnit getTimeUnitByName(String name) {
         return Arrays.stream(ETimeUnit.values())
-                .filter(timeUnit -> timeUnit.getDesc().equals(name))
-                .findFirst()
-                .orElseThrow(() -> new RequestException(HttpStatus.NOT_FOUND, String.format(Messages.Error.TIME_UNIT_NOT_FOUND, name)));
+            .filter(timeUnit -> timeUnit.getDesc().equals(name))
+            .findFirst()
+            .orElseThrow(() -> new RequestException(HttpStatus.NOT_FOUND, String.format(Messages.Error.TIME_UNIT_NOT_FOUND, name)));
     }
 
 }
