c216383d841b7d0e64edcdc4d35aa00e72c1a213
L <eleaz.rs@gmail.com>
2024-02-05 18:09:21 +0100

    limit mapping details for group

--- src/main/java/org/ldcgc/backend/payload/mapper/resources/consumable/ConsumableMapper.java
+++ src/main/java/org/ldcgc/backend/payload/mapper/resources/consumable/ConsumableMapper.java
@@ -5,6 +5,7 @@ import org.ldcgc.backend.payload.dto.resources.ConsumableDto;
 import org.ldcgc.backend.payload.mapper.category.CategoryMapper;
 import org.ldcgc.backend.payload.mapper.location.LocationMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
@@ -15,8 +16,11 @@ public interface ConsumableMapper {
 
     ConsumableMapper MAPPER = Mappers.getMapper(ConsumableMapper.class);
 
+    @Mapping(target = "group.location.locations", ignore = true)
     ConsumableDto toDto(Consumable consumable);
+
     Consumable toMo(ConsumableDto consumableDto);
+
     void update(ConsumableDto from, @MappingTarget Consumable to);
 
 }
