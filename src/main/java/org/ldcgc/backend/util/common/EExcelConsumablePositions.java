01c83bfdfa67a5d515fd8f1ce9bdd5b7f899f8f0
L <eleaz.rs@gmail.com>
2024-02-05 12:25:44 +0100

    add Excel Consumable Positions enum

---
+++ src/main/java/org/ldcgc/backend/util/common/EExcelConsumablePositions.java
@@ -0,0 +1,26 @@
+package org.ldcgc.backend.util.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum EExcelConsumablePositions {
+
+    BRAND(0),
+    CATEGORY(1),
+    GROUP(2),
+    LOCATION(3),
+    MIN_STOCK(4),
+    PRICE(5),
+    PURCHASE_DATE(6),
+    STOCK(7),
+    STOCK_TYPE(8),
+    BARCODE(9),
+    DESCRIPTION(10),
+    MODEL(11),
+    NAME(12),
+    URL_IMAGES(13);
+
+    private final Integer columnNumber;
+}
