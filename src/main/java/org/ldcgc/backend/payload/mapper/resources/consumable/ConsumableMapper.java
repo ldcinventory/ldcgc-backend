eaf0dec03d3ecaa7eb196a0c899d4d93ab09cbea
L <eleaz.rs@gmail.com>
2024-02-06 14:29:36 +0100

    refine consumable mapper

--- src/main/java/org/ldcgc/backend/payload/mapper/resources/consumable/ConsumableMapper.java
+++ src/main/java/org/ldcgc/backend/payload/mapper/resources/consumable/ConsumableMapper.java
@@ -4,29 +4,32 @@ import org.ldcgc.backend.db.model.resources.Consumable;
 import org.ldcgc.backend.payload.dto.resources.ConsumableDto;
 import org.ldcgc.backend.payload.mapper.category.CategoryMapper;
 import org.ldcgc.backend.payload.mapper.location.LocationMapper;
-import org.ldcgc.backend.util.common.EStockType;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = { LocationMapper.class, CategoryMapper.class },
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(uses = LocationMapper.class, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ConsumableMapper {
 
     ConsumableMapper MAPPER = Mappers.getMapper(ConsumableMapper.class);
 
     @Mapping(target = "group.location.locations", ignore = true)
-    @Mapping(target = "stockType", source = "stockType", qualifiedByName = "mapStockTypeToDto")
+    @Mapping(target = "location.locations", ignore = true)
+    @Mapping(target = "location.parent.locations", ignore = true)
+    @Mapping(target = "category.locked", ignore = true)
+    @Mapping(target = "category.categories", ignore = true)
+    @Mapping(target = "category.parent.locked", ignore = true)
+    @Mapping(target = "brand.locked", ignore = true)
+    @Mapping(target = "brand.categories", ignore = true)
+    @Mapping(target = "brand.parent.locked", ignore = true)
     ConsumableDto toDto(Consumable consumable);
 
     @Mapping(target = "brand", ignore = true)
     @Mapping(target = "category", ignore = true)
     @Mapping(target = "location", ignore = true)
     @Mapping(target = "group", ignore = true)
-    @Mapping(target = "stockType", source = "stockType", qualifiedByName = "mapStockTypeToMo")
     Consumable toMo(ConsumableDto consumableDto);
 
     @Mapping(target = "brand", ignore = true)
@@ -34,15 +37,5 @@ public interface ConsumableMapper {
     @Mapping(target = "location", ignore = true)
     @Mapping(target = "group", ignore = true)
     void update(ConsumableDto from, @MappingTarget Consumable to);
-
-    @Named("mapStockTypeToMo")
-    static EStockType mapStockTypeToMo(String stockTypeName){
-        return EStockType.getStockTypeByName(stockTypeName);
-    }
-
-    @Named("mapStockTypeToDto")
-    static String mapStockTypeToDto(EStockType stockTypeName){
-        return stockTypeName.getAbbr();
-    }
 
 }
