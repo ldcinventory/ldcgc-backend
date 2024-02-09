c1413d54499babd1064c6adbfd5fb57f198a5d30
L <eleaz.rs@gmail.com>
2024-02-09 18:03:42 +0100

    lighten the load of ToolDTO when mapping in response

--- src/main/java/org/ldcgc/backend/payload/mapper/resources/tool/ToolMapper.java
+++ src/main/java/org/ldcgc/backend/payload/mapper/resources/tool/ToolMapper.java
@@ -5,6 +5,7 @@ import org.ldcgc.backend.payload.dto.resources.ToolDto;
 import org.ldcgc.backend.payload.mapper.category.CategoryMapper;
 import org.ldcgc.backend.payload.mapper.location.LocationMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
@@ -17,9 +18,15 @@ public interface ToolMapper {
 
     ToolMapper MAPPER = Mappers.getMapper(ToolMapper.class);
 
+    @Mapping(target = "location.locations", ignore = true)
+    @Mapping(target = "location.parent.locations", ignore = true)
+    @Mapping(target = "group.location.locations", ignore = true)
     ToolDto toDto(Tool tool);
+
     Tool toMo(ToolDto toolDto);
+
     List<Tool> toMo(List<ToolDto> tools);
+
     void update(ToolDto from, @MappingTarget Tool to);
 
 }
