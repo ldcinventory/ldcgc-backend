426d91e73b06e8633a30fb3af1b7f691cfcbf1dd
L <eleaz.rs@gmail.com>
2024-02-21 11:40:07 +0100

    add new field on PaginationDetails

--- src/main/java/org/ldcgc/backend/payload/dto/other/PaginationDetails.java
+++ src/main/java/org/ldcgc/backend/payload/dto/other/PaginationDetails.java
@@ -6,24 +6,29 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 @Getter
-@Builder
+@Builder(toBuilder = true)
 public class PaginationDetails {
 
     private int numElements;
     private int elementsPerPage;
+    private int elementsThisPage;
     private int actualPage;
     private int actualPageFrom;
     private int actualPageTo;
     private int totalPages;
 
     public static PaginationDetails fromPaging(Pageable pageable, Page<?> page) {
-        return PaginationDetails.builder()
+        PaginationDetails paginationDetails = PaginationDetails.builder()
             .numElements((int) page.getTotalElements())
             .elementsPerPage(pageable.getPageSize())
             .actualPage(pageable.getPageNumber())
             .actualPageFrom(pageable.getPageNumber() * pageable.getPageSize() + 1)
             .actualPageTo(Math.min(((pageable.getPageNumber() + 1) * pageable.getPageSize()), (int) page.getTotalElements()))
             .totalPages(page.getTotalPages())
+            .build();
+
+        return paginationDetails.toBuilder()
+            .elementsThisPage(paginationDetails.getActualPageTo() - paginationDetails.getActualPageFrom() + 1)
             .build();
     }
 
