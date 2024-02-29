fae27181a5f0a4d648fb58fc36d984b34511eb36
srello <srello0410@gmail.com>
2024-02-29 15:49:58 +0100

    fixes

--- src/main/java/org/ldcgc/backend/payload/mapper/history/tool/ToolRegisterMapper.java
+++ src/main/java/org/ldcgc/backend/payload/mapper/history/tool/ToolRegisterMapper.java
@@ -3,11 +3,12 @@ package org.ldcgc.backend.payload.mapper.history.tool;
 
 import org.ldcgc.backend.db.model.history.ToolRegister;
 import org.ldcgc.backend.payload.dto.history.ToolRegisterDto;
+import org.ldcgc.backend.payload.mapper.resources.tool.ToolMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(uses = ToolMapper.class)
 public interface ToolRegisterMapper {
     ToolRegisterMapper MAPPER = Mappers.getMapper(ToolRegisterMapper.class);
 
