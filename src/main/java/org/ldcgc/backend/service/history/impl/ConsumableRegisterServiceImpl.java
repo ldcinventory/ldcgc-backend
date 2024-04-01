ebd3a8d74409032dc63268346ea5ddcdba92c23c
srello <srello0410@gmail.com>
2024-04-01 16:17:34 +0200

    Included desc order filter in find all consumables register

--- src/main/java/org/ldcgc/backend/service/history/impl/ConsumableRegisterServiceImpl.java
+++ src/main/java/org/ldcgc/backend/service/history/impl/ConsumableRegisterServiceImpl.java
@@ -45,12 +45,14 @@ public class ConsumableRegisterServiceImpl implements ConsumableRegisterService 
         return Constructor.buildResponseObject(HttpStatus.OK, ConsumableRegisterMapper.MAPPER.toDto(consumableRegister));
     }
 
-    public ResponseEntity<?> listConsumableRegister(Integer pageIndex, Integer size, String volunteer, String consumable, LocalDateTime registerFrom, LocalDateTime registerTo, ERegisterStatus status, String sortField, boolean descOrder) {
-        Pageable pageable = PageRequest.of(pageIndex, size, Sort.by(sortField).ascending());
+    public ResponseEntity<?> listConsumableRegister(Integer pageIndex, Integer size, String volunteer, String consumable,
+                                                    LocalDateTime registerFrom, LocalDateTime registerTo, ERegisterStatus status, String sortField, boolean descOrder) {
+        Pageable pageable = PageRequest.of(pageIndex, size, descOrder ? Sort.by(sortField).descending() : Sort.by(sortField).ascending());
 
         Page<ConsumableRegisterDto> pagedConsumableRegisters = ObjectUtils.allNull(volunteer, consumable, registerFrom, registerTo, status)
             ? consumableRegisterRepository.findAll(pageable).map(ConsumableRegisterMapper.MAPPER::toDto)
-            : consumableRegisterRepository.findAllFiltered(Optional.ofNullable(status).map(ERegisterStatus::getName).orElse(null), volunteer, consumable, registerFrom, registerTo, pageable).map(ConsumableRegisterMapper.MAPPER::toDto);
+            : consumableRegisterRepository.findAllFiltered(Optional.ofNullable(status).map(ERegisterStatus::getName).orElse(null),
+                volunteer, consumable, registerFrom, registerTo, pageable).map(ConsumableRegisterMapper.MAPPER::toDto);
 
         if (pageIndex > pagedConsumableRegisters.getTotalPages())
             throw new RequestException(HttpStatus.BAD_REQUEST, Messages.Error.PAGE_INDEX_REQUESTED_EXCEEDED_TOTAL);
