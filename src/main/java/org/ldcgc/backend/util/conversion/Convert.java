d95f79bf1cd85d607ebb13bdb8f6366e72cb3d79
L <eleaz.rs@gmail.com>
2024-04-09 13:31:42 +0200

    fix NPEs in Convert methods

--- src/main/java/org/ldcgc/backend/util/conversion/Convert.java
+++ src/main/java/org/ldcgc/backend/util/conversion/Convert.java
@@ -1,5 +1,6 @@
 package org.ldcgc.backend.util.conversion;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigDecimal;
@@ -10,6 +11,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Optional;
 
 public class Convert {
 
@@ -22,7 +24,9 @@ public class Convert {
     }
 
     public static LocalDate stringToLocalDate(String dbString, String pattern) {
-        return LocalDate.parse(dbString, DateTimeFormatter.ofPattern(pattern));
+        return dbString.matches(pattern)
+            ? LocalDate.parse(dbString, DateTimeFormatter.ofPattern(pattern))
+            : null;
     }
 
     public static Float toFloat2Decimals(String number) {
@@ -43,11 +47,15 @@ public class Convert {
     }
 
     public static LocalDateTime dateToLocalDateTime(Date dateToConvert) {
-        return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        return Optional.ofNullable(dateToConvert)
+            .map(d -> d.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
+            .orElse(null);
     }
 
     public static LocalDate dateToLocalDate(Date dateToConvert) {
-        return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return Optional.ofNullable(dateToConvert)
+            .map(d -> d.toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+            .orElse(null);
     }
 
 }
