3fdef1b1d0e119bfe960cd44fddd6d461d3eb20f
L <eleaz.rs@gmail.com>
2024-04-18 09:09:35 +0200

    clean imports

--- src/main/java/org/ldcgc/backend/db/repository/category/ResponsibilityRepository.java
+++ src/main/java/org/ldcgc/backend/db/repository/category/ResponsibilityRepository.java
@@ -3,7 +3,6 @@ package org.ldcgc.backend.db.repository.category;
 import org.ldcgc.backend.db.model.category.Responsibility;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface ResponsibilityRepository extends JpaRepository<Responsibility, Integer> {
--- src/main/java/org/ldcgc/backend/payload/dto/history/ConsumableRegisterDto.java
+++ src/main/java/org/ldcgc/backend/payload/dto/history/ConsumableRegisterDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
-import org.ldcgc.backend.payload.dto.resources.ConsumableDto;
 import org.ldcgc.backend.payload.dto.users.VolunteerDto;
 
 import java.time.LocalDateTime;
