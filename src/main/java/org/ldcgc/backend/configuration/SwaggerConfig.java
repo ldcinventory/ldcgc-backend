1d67e933ba9e170ea774f153aaf7ff54b04c3777
L <eleaz.rs@gmail.com>
2024-04-18 08:29:51 +0200

    add new line for roles accepted in Swagger descriptions

--- src/main/java/org/ldcgc/backend/configuration/SwaggerConfig.java
+++ src/main/java/org/ldcgc/backend/configuration/SwaggerConfig.java
@@ -44,10 +44,10 @@ public class SwaggerConfig {
         HTTP_REASON_501 = "Not Implemented";
 
     // Swagger constants
-    public static final String SWAGGER_ROLE_OPERATION_ADMIN = "Roles addmited: <b>ADMIN</b>",
-        SWAGGER_ROLE_OPERATION_MANAGER = "Roles addmited: <b>ADMIN</b>, <b>MANAGER</b>",
-        SWAGGER_ROLE_OPERATION_USER = "Roles addmited: <b>ADMIN</b>, <b>MANAGER</b>, <b>STANDARD</b>",
-        SWAGGER_ROLE_OPERATION_NON_LOGGED = "Roles addmited: <b>Non-logged user</b>";
+    public static final String SWAGGER_ROLE_OPERATION_ADMIN = "\nRoles admitted: <b>ADMIN</b>",
+        SWAGGER_ROLE_OPERATION_MANAGER = "\nRoles admitted: <b>ADMIN</b>, <b>MANAGER</b>",
+        SWAGGER_ROLE_OPERATION_USER = "\nRoles admitted: <b>ADMIN</b>, <b>MANAGER</b>, <b>STANDARD</b>",
+        SWAGGER_ROLE_OPERATION_NON_LOGGED = "\nRoles admitted: <b>Non-logged user</b>";
 
     @Bean
     public OpenAPI gc8InventoryAPI() {
