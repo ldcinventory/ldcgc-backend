52b44bc6aeacaa38d2b128de8fc0b94dd5d36346
L <eleaz.rs@gmail.com>
2024-04-22 10:45:45 +0200

    add profiles to test only in DEV

--- src/main/java/org/ldcgc/backend/controller/test/TestController.java
+++ src/main/java/org/ldcgc/backend/controller/test/TestController.java
@@ -1,5 +1,6 @@
 package org.ldcgc.backend.controller.test;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -14,14 +15,17 @@ import static org.ldcgc.backend.security.Authority.Role.USER_LEVEL;
 @RequestMapping("/test")
 public interface TestController {
 
+    @Profile("dev")
     @GetMapping("/user")
     @PreAuthorize(USER_LEVEL)
     ResponseEntity<?> testAccessWithCredentials();
 
+    @Profile("dev")
     @GetMapping("/manager")
     @PreAuthorize(MANAGER_LEVEL)
     ResponseEntity<?> testAccessWithManagerCredentials();
 
+    @Profile("dev")
     @GetMapping("/admin")
     @PreAuthorize(ADMIN_LEVEL)
     ResponseEntity<?> testAccessWithAdminCredentials();
