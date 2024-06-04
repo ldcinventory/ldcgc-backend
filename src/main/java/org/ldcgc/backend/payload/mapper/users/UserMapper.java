d157828e80163c80ac1ad28de57838cf30609df3
L <eleaz.rs@gmail.com>
2024-06-04 07:51:51 -0400

    ignore some details for user (volunteer -> group), it's the same as user

--- src/main/java/org/ldcgc/backend/payload/mapper/users/UserMapper.java
+++ src/main/java/org/ldcgc/backend/payload/mapper/users/UserMapper.java
@@ -3,6 +3,7 @@ package org.ldcgc.backend.payload.mapper.users;
 import org.ldcgc.backend.db.model.users.User;
 import org.ldcgc.backend.payload.dto.users.UserCredentialsDto;
 import org.ldcgc.backend.payload.dto.users.UserDto;
+import org.ldcgc.backend.payload.dto.users.VolunteerDto;
 import org.ldcgc.backend.payload.mapper.location.LocationMapper;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
@@ -21,10 +22,11 @@ public interface UserMapper {
 
     @Mapping(source = "password", target = "password", qualifiedByName = "mapPasswordToEncryptedPassword")
     @Mapping(target = "group.location.locations", ignore = true)
-    User toEntity(UserDto userRequest);
+    User toEntity(UserDto userDto);
 
     @Mapping(target = "password", ignore = true)
     @Mapping(target = "volunteer.absences", ignore = true)
+    @Mapping(target = "volunteer.group", ignore = true)
     UserDto toDTO(User user);
 
     UserCredentialsDto toCredentialsDTO(User user);
